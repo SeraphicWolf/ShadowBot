0ec08a323a3e183ea802e82ac4b54755cb323a2d
Sebastion <masterofpika11@gmail.com>
2018-02-28 21:38:33 -0600

    Update index.js

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ var fortunes = [
 
 var bot = new Discord.Client();
 
-bot.on("ready", function() {
+client.on("ready", function() {
     console.log("Ready");
 });
 
-bot.on("message", function(message) {
+client.on("message", function(message) {
     if (message.author.equals(bot.user)) return;
 
     if (!message.content.startsWith(PREFIX)) return;
