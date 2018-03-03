f8d6c95b5b0ff7526f17aaaf17ffe4ecd58779e5
Sebastion <masterofpika11@gmail.com>
2018-03-02 23:29:22 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,50 +1,47 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-
 const PREFIX = "!"
 
-var fortunes = [
-    "Yes",
-    "No",
-    "Mabye",
-    "Hell No",
-];
-
-var bot = new Discord.Client();
-
-client.on("ready", function() {
-    console.log("Ready");
+var fortunes = ["Yes", "No", "Mabye", "Hell No"];
+client.on("ready", () => {
+  console.log("Ready");
+  client.user.setActivity("Horror Games", {type: 'PLAYING'});
 });
 
-client.on("message", function(message) {
-    if (message.author.equals(bot.user)) return;
+client.on("message", message => {
+  const args = message.content.split(' ');
+  let command = message.content.toLowerCase().split(" ")[0];
+  command = command.slice(prefix.length)
+  
+  if (message.author.bot) return;
+  if (!message.content.startsWith(PREFIX)) return;
 
-    if (!message.content.startsWith(PREFIX)) return;
+  switch (command) {
+    case "ping":
+       message.channel.sendMessage("Pong!");
+    break;
 
-    var args = message.content.substring(PREFIX.length).split(" ");
+    case "botinfo":
+      var embed = new Discord.RichEmbed()
+      .addField("This Bot Was Created on 2/28/2018 at 9:59 PM")
+      message.channel.sendEmbed(embed);
+    break;
 
-    switch (args[0]) {
-        case "ping":
-           message.channel.sendMessage("Pong!");
-           break;
-        case "botinfo":
-          var embed = new Discord.RichEmbed()
-           .addField("This Bot Was Created on 2/28/2018 at 9:59 PM")
-           message.channel.sendEmbed(embed);
-           break;
-        case "8ball":
-          if (args[1]) message.channel.sendMessage(fortunes[Math.floor(Math.random() * fortunes.length)]);
-          else message.channel.sendMessage("I can't read that");
-          break;
-        case "help":
-           var embed = new Discord.RichEmbed()
-            .addField("ping","Pong!")
-            .addField("botinfo","Display's Info about the bot")
-            .addField("8ball","Tell's The Future!")
-            message.channel.sendEmbed(embed);
-            break;
-        default:
-           message.channel.sendMessage("Invalid Command");
+    case "8ball":
+      if (args[1]) message.channel.sendMessage(fortunes[Math.floor(Math.random() * fortunes.length)]);
+      else message.channel.sendMessage("I can't read that");
+    break;
+
+    case "help":
+      var embed = new Discord.RichEmbed()
+      .addField("ping","Pong!")
+      .addField("botinfo","Display's Info about the bot")
+      .addField("8ball","Tell's The Future!")
+      message.channel.sendEmbed(embed);
+    break;
+
+    default:
+      message.channel.sendMessage("Invalid Command");
     }
 });
 
